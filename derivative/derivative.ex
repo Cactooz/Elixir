36ca5e239ee5f20bcf6e3ce477df204e085d557e
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-01-18 15:50:21 +0100

    Add division and subtraction derivative

--- derivative/derivative.ex
+++ derivative/derivative.ex
@@ -3,7 +3,9 @@ defmodule Derivative do
 
   @type expr() :: literal() |
   {:add, expr(), expr()} |
+  {:sub, expr(), expr()} |
   {:mul, expr(), expr()} |
+  {:div, expr(), expr()} |
   {:exp, expr(), {:num, literal()}} |
   {:ln, expr()}
 
@@ -25,6 +27,12 @@ defmodule Derivative do
     IO.write("Derivative: #{print(d)} for expression #{print(e)}")
   end
 
+  def div() do
+    e = {:div, {:add, {:num, 1}, {:var, :x}}, {:sub, {:var, :x}, {:num, 2}}}
+    d = derive(e, :x)
+    IO.write("Derivative: #{print(d)} for expression #{print(e)}")
+  end
+
   def derive({:num, _}, _) do {:num, 0} end
 
   def derive({:var, v}, v) do {:num, 1} end
@@ -35,8 +43,20 @@ defmodule Derivative do
     {:add, derive(e1, v), derive(e2, v)}
   end
 
+  def derive({:sub, e1, e2}, v) do
+    {:sub, derive(e1, v), derive(e2, v)}
+  end
+
   def derive({:mul, e1, e2}, v) do
     {:add, {:mul, derive(e1, v), e2}, {:mul, e1, derive(e2, v)}}
+  end
+
+  def derive({:div, top, div}, v) do
+    {:div,
+    {:sub,
+      {:mul, derive(top, v), div},
+      {:mul, top, derive(div, v)}},
+    {:exp, div, {:num, 2}}}
   end
 
   def derive({:exp, base, {:num, exp}}, v) do
@@ -50,6 +70,7 @@ defmodule Derivative do
   def print({:num, n}) do "#{n}" end
   def print({:var, v}) do "#{v}" end
   def print({:add, e1, e2}) do "(#{print(e1)} + #{print(e2)})" end
+  def print({:sub, e1, e2}) do "(#{print(e1)} - #{print(e2)})" end
   def print({:mul, e1, e2}) do "#{print(e1)} * #{print(e2)}" end
   def print({:div, top, div}) do "(#{print(top)}) / (#{print(div)})" end
   def print({:exp, base, exp}) do "(#{print(base)}) ^ (#{print(exp)})" end
