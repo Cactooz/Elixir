7cbf1bfc5e228d4fad40a89313805ea0632fd7d2
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-01-30 16:10:43 +0100

    Fix :q not using :num

--- evaluation/evaluation.ex
+++ evaluation/evaluation.ex
@@ -10,51 +10,33 @@ defmodule Evaluation do
   def eval(env, {:div, e1, e2}) do d(eval(env, e1), eval(env, e2)) end
   def eval(_, {:q, e1, e2}) do reduce(e1, e2) end
 
-  def add({:q, {:num, n1}, {:num, d1}}, {:q, {:num, n2}, {:num, d2}}) do
-    {:q, {:num, (n1*d2)+(n2*d1)}, {:num, d1*d2}}
-  end
-  def add({:num, n1}, {:q, {:num, n2}, {:num, d}}) do
-    {:q, {:num, (n1*d)+n2}, {:num, d}}
-  end
-  def add({:q, {:num, n1}, {:num, d}}, {:num, n2}) do
-    {:q, {:num, n1+(n2*d)}, {:num, d}}
-  end
+  def add({:q, n1, d1}, {:q, n2, d2}) do {:q, (n1*d2)+(n2*d1), d1*d2} end
+  def add({:num, n1}, {:q, n2, d}) do {:q, (n1*d)+n2, d} end
+  def add({:q, n1, d}, {:num, n2}) do {:q, n1+(n2*d), d} end
   def add({:num, n1}, {:num, n2}) do {:num, n1+n2} end
 
-  def sub({:q, {:num, n1}, {:num, d1}}, {:q, {:num, n2}, {:num, d2}}) do
-    {:q, {:num, (n1*d2)-(n2*d1)}, {:num, d1*d2}}
-  end
-  def sub({:num, n1}, {:q, {:num, n2}, {:num, d}}) do
-    {:q, {:num, (n1*d)-n2}, {:num, d}}
-  end
-  def sub({:q, {:num, n1}, {:num, d}}, {:num, n2}) do
-    {:q, {:num, n1-(n2*d)}, {:num, d}}
-  end
+  def sub({:q, n1, d1}, {:q, n2, d2}) do {:q, (n1*d2)-(n2*d1), d1*d2} end
+  def sub({:num, n1}, {:q, n2, d}) do {:q, (n1*d)-n2, d} end
+  def sub({:q, n1, d}, {:num, n2}) do {:q, n1-(n2*d), d} end
   def sub({:num, n1}, {:num, n2}) do {:num, n1-n2} end
 
-  def mul({:q, {:num, n1}, {:num, d1}}, {:q, {:num, n2}, {:num, d2}}) do
-    {:q, {:num, n1*n2}, {:num, d1*d2}}
-  end
-  def mul({:num, n1}, {:q, {:num, n2}, {:num, d}}) do
-    {:q, {:num, n1*n2}, {:num, d}}
-  end
-  def mul({:q, {:num, n1}, {:num, d}}, {:num, n2}) do
-    {:q, {:num, n1*n2, d}}
-  end
+  def mul({:q, n1, d1}, {:q, n2, d2}) do {:q, n1*n2, d1*d2} end
+  def mul({:num, n1}, {:q, n2, d}) do {:q, n1*n2, d} end
+  def mul({:q, n1, d}, {:num, n2}) do {:q, n1*n2, d} end
   def mul({:num, n1}, {:num, n2}) do {:num, n1*n2} end
 
   def d({:num, n1}, {:num, n2}) do
     if rem(n1, n2) == 0 do
       {:num, trunc(n1/n2)}
     else
-      {:q, {:num, n1}, {:num, n2}}
+      {:q, n1, n2}
     end
   end
 
   def gcd(n, 0) do n end
-  def gcd({:num, n1}, {:num, n2}) do {:num, gcd(n2, rem(n1, n2))} end
+  def gcd(n1, n2) do gcd(n2, rem(n1, n2)) end
 
-  def reduce({:num, e1}, {:num, e2}) do
-    {:q, {:num, trunc(e1/gcd(e1,e2))}, {:num, trunc(e2/gcd(e1,e2))}}
+  def reduce(e1, e2) do
+    {:q, trunc(e1/gcd(e1,e2)), trunc(e2/gcd(e1,e2))}
   end
 end
