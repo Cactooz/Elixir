1e0220944b3a1effdf57766a45cf9b9d87fbdd08
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-01-31 11:43:54 +0100

    Add eval_match to eager.ex

--- interpreter/eager.ex
+++ interpreter/eager.ex
@@ -18,4 +18,25 @@ defmodule Eager do
     end
   end
 
+  def eval_match(:ignore, _, env) do {:ok, env} end
+
+  def eval_match({:atm, id}, id, env) do {:ok, env} end
+
+  def eval_match({:var, id}, str, env) do
+    case Env.lookup(id, env) do
+      nil -> {:ok, Env.add(id, str, env)}
+      {_, ^str} -> {:ok, env}
+      {_, _} -> :fail
+    end
+  end
+
+  def eval_match({:cons, hp, tp}, {hs, ts}, env) do
+    case eval_match(hp, hs, env) do
+      :fail -> :fail
+      {_, tenv} -> eval_match(tp, ts, tenv)
+    end
+  end
+
+  def eval_match(_, _, _) do :fail end
+
 end
