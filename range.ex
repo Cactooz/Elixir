f5f5c150632e5a20e5f986006de0375a6335e0b3
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-09 15:13:58 +0100

    Add generic functions using reduce for range.ex

--- range.ex
+++ range.ex
@@ -11,6 +11,7 @@ defmodule Range do
   end
   """
 
+  """
   def sum(range) do sum(range, 0) end
   def sum({:range, from, to}, sum) do
     if(from <= to) do
@@ -64,5 +65,44 @@ defmodule Range do
     else
       acc
     end
+  end
+  """
+
+  def reduce({:range, from, to}, {:cont, acc}, f) do
+    if(from <= to) do
+      reduce({:range, from+1, to}, f.(from, acc), f)
+    else
+      {:done, acc}
+    end
+  end
+  def reduce(_, {:halt, acc}, _) do
+    {:halted, acc}
+  end
+
+  def sum(range) do reduce(range, {:cont, 0}, fn(x,sum) -> {:cont, x+sum} end) end
+
+  def map(range, f) do
+    reduce(range, {:cont, []}, fn(x, acc) -> {:cont, [f.(x)|acc]} end)
+  end
+
+  def filter(range, f) do
+    reduce(range, {:cont, []}, fn(x, acc) ->
+      if(f.(x)) do
+        {:cont, [x|acc]}
+      else
+        {:cont, acc}
+      end
+    end)
+  end
+
+  def take(range, n) do
+    reduce(range, {:cont, {:sofar, n, []}},
+      fn(x, {:sofar, n, acc}) ->
+        if(n>0) do
+          {:cont, {:sofar, n-1, [x|acc]}}
+        else
+          {:halt, acc}
+        end
+      end)
   end
 end
