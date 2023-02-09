93798088aebc1e08b23f30b6e1ef476b1a614d63
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-09 15:00:28 +0100

    Add more range functions for manipulation

--- range.ex
+++ range.ex
@@ -20,4 +20,49 @@ defmodule Range do
     end
   end
 
+
+  def foldl({:range, from, to}, sum, f) do
+    if(from <= to) do
+      foldl({:range, from+1, to}, f.(from, sum), f)
+    else
+      sum
+    end
+  end
+
+  def map(range, f) do
+    Enum.reverse(map(range, [], f))
+  end
+  def map({:range, from, to}, acc, f) do
+    if(from <= to) do
+      map({:range, from+1, to}, [f.(from)|acc], f)
+    end
+  end
+
+  def filter(range, f) do
+    Enum.reverse(filter(range, [], f))
+  end
+  def filter({:range, from, to}, acc, f) do
+    if(from <= to) do
+      if(f.(from)) do
+        filter({:range, from+1, to}, [from|acc], f)
+      else
+        filter({:range, from+1, to}, acc, f)
+      end
+    else
+      acc
+    end
+  end
+
+  def take(range, n) do Enum.reverse(take(range, [], n)) end
+  def take({:range, from, to}, acc, n) do
+    if(from <= to) do
+      if(n > 0) do
+        take({:range, from+1, to}, [from|acc], n-1)
+      else
+        acc
+      end
+    else
+      acc
+    end
+  end
 end
