ae55e5623397e9f733c0f7102f4fd145c6dc7aac
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-09 14:56:18 +0100

    Add new sum function to range.ex

--- range.ex
+++ range.ex
@@ -1,11 +1,22 @@
 defmodule Range do
   def new(from, to) do {:range, from, to} end
 
+  """
   def sum({:range, from, to}) do
     if(from <= to) do
       from + sum({:range, from+1, to})
     else
       0
+    end
+  end
+  """
+
+  def sum(range) do sum(range, 0) end
+  def sum({:range, from, to}, sum) do
+    if(from <= to) do
+      sum({:range, from+1, to}, sum+from)
+    else
+      sum
     end
   end
 
