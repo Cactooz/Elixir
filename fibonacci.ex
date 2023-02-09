7f8f0d7238a95a15b66da7c5b94f1f641e6d034f
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-09 13:29:28 +0100

    Add fibonacci with functions

--- fibonacci.ex
+++ fibonacci.ex
@@ -1,11 +1,16 @@
 defmodule Fibonacci do
   def fib(0) do 0 end
-
   def fib(1) do 1 end
-
   def fib(n) do
     fib(n - 1) + fib(n - 2)
   end
 end
 
-IO.puts(Fibonacci.fib(8))
+defmodule Fibonacci2 do
+  def fib() do
+    fn() -> fib(0,1) end
+  end
+  def fib(f1, f2) do
+    [f1|fn() -> fib(f2,f1+f2) end]
+  end
+end
