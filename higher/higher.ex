66ead1c478ce1c1b448eb68d18cc87db3f254dcd
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-10 13:43:50 +0100

    Add sum and odd functions to higher.ex

--- higher/higher.ex
+++ higher/higher.ex
@@ -9,6 +9,20 @@ defmodule Higher do
     [head+n|add(tail, n)]
   end
 
+  def sum([]) do 0 end
+  def sum([head|tail]) do
+    head + sum(tail)
+  end
+
+  def odd([]) do [] end
+  def odd([head|tail]) do
+    if(rem(head,2) == 1) do
+      [head|odd(tail)]
+    else
+      odd(tail)
+    end
+  end
+
   def apply_to_all([], _) do [] end
   def apply_to_all([head|tail], f) do
     [f.(head)|apply_to_all(tail, f)]
