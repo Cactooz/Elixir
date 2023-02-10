08ac171dc2db2e5770f7d6331b1d9b8facfb97f2
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-10 13:49:53 +0100

    Add fold right and left to higher.ex

--- higher/higher.ex
+++ higher/higher.ex
@@ -27,4 +27,14 @@ defmodule Higher do
   def apply_to_all([head|tail], f) do
     [f.(head)|apply_to_all(tail, f)]
   end
+
+  def fold_right([], acc, _) do acc end
+  def fold_right([head|tail], acc, f) do
+    f.(head, fold_right(tail, acc, f))
+  end
+
+  def fold_left([], acc, _) do acc end
+  def fold_left([head|tail], acc, f) do
+    fold_left(tail, f.(head, acc), f)
+  end
 end
