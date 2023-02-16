1245c79566c0096085d3e00d86296490c0fcaad5
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-16 15:03:29 +0100

    Add faster splitter for dynamic.ex

--- dynamic.ex
+++ dynamic.ex
@@ -23,13 +23,13 @@ defmodule Dynamic do
     end)
   end
 
-  def split([x|seq]) do split(seq, x, [x], []) end
+  def split([x|seq]) do split(seq, x, [x], [], []) end
 
-  def split([], length, left, right) do [{left, right, length}] end
-  def split([x], length, [], right) do [{[x], right, length+x}] end
-  def split([x], length, left, []) do [{left, [x], length+x}] end
-  def split([x|seq], length, left, right) do
-    split(seq, length+x, [x|left], right) ++ split(seq, length+x, left, [x|right])
+  def split([], length, left, right, acc) do [{Enum.reverse(left), Enum.reverse(right), length}|acc] end
+  def split([x], length, [], right, acc) do [{[x], Enum.reverse(right), length+x}|acc] end
+  def split([x], length, left, [], acc) do [{Enum.reverse(left), [x], length+x}|acc] end
+  def split([x|seq], length, left, right, acc) do
+    split(seq, length+x, [x|left], right, split(seq, length+x, left, [x|right], acc))
   end
 
 end
