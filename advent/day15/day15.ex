444676509b9dca9fee0a7605b65565484f39d98b
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-18 19:44:03 +0100

    Add advent day15 for sample input

---
+++ advent/day15/day15.ex
@@ -0,0 +1,58 @@
+defmodule Day15 do
+  def part1() do
+    sensors = Enum.map(sample(), fn(row) -> parse(row) end)
+    y = 10
+    row = Row.new()
+    row = Enum.reduce(sensors, row, fn({:sensor, sensor, beacon}, row) ->
+      block(sensor, beacon, y, row)
+    end)
+    Row.blocked(row)
+  end
+
+  def block({sx, sy}, {bx, by}, y, row) do
+    dist = abs(sx - bx) +  abs(sy - by)
+    diff = dist - abs(sy - y)
+    if(diff >= 0) do
+      if(y == by) do
+        cond do
+          sx < bx ->
+            Row.block(row, (sx-diff), (sx+diff-1))
+          sx > bx ->
+            Row.block(row, (sx-diff+1), (sx+diff))
+          sx == bx ->
+            row
+        end
+      else
+        Row.block(row, (sx-diff), (sx+diff))
+      end
+    else
+      row
+    end
+  end
+
+  def sample() do
+    ["Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+     "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+     "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+     "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+     "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+     "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+     "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+     "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+     "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+     "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+     "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+     "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+     "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+     "Sensor at x=20, y=1: closest beacon is at x=15, y=3"]
+  end
+
+  def parse(input) do
+    [_, sx, sy, _, bx, by] = String.split(input, [":","="])
+    {sx, _} = Integer.parse(sx)
+    {sy, _} = Integer.parse(sy)
+    {bx, _} = Integer.parse(bx)
+    {by, _} = Integer.parse(by)
+    {:sensor, {sx, sy}, {bx, by}}
+  end
+end
