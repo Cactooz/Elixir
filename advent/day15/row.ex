a23a28412369a2c860acbc28637781c5b1d6620a
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-18 19:58:27 +0100

    Change Row to use intervals

--- advent/day15/row.ex
+++ advent/day15/row.ex
@@ -1,9 +1,18 @@
 defmodule Row do
-  def new() do MapSet.new() end
+  def new() do [] end
 
-  def block(row, from, to) do
-    Enum.reduce(from..to, row, fn(i, r) -> MapSet.put(r, i) end)
+  def block([], from, to) do [{from, to}] end
+  def block([{f, t}|rest], from, to) when from > (t+1) do
+    [{f, t}|block(rest, from, to)]
+  end
+  def block([{f, _t}|_]=rest, from, to) when to < (f-1) do
+    [{from,to}|rest]
+  end
+  def block([{f, t}|rest], from, to) do
+    block(rest, min(f, from), max(t, to))
   end
 
-  def blocked(row) do MapSet.size(row) end
+  def blocked(row) do
+    row
+  end
 end
