21924197b421969708e3cb541adeb2a452009e6a
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-23 23:22:06 +0100

    Create philosopher.ex

---
+++ philosophers/philosopher.ex
@@ -0,0 +1,33 @@
+defmodule Philosopher do
+  def start(hunger, left, right, name, controller) do
+    spawn_link(fn() -> dream(hunger, left, right, name, controller) end)
+  end
+
+  def dream(0, left, right, name, controller) do
+    send(controller, :done)
+  end
+  def dream(hunger, left, right, name, controller) do
+    sleep(300)
+    wait(hunger, left, right, name, controller)
+  end
+
+  def wait(hunger, left, right, name, controller) do
+    case Chopstick.request(left) do
+      :ok ->
+        case Chopstick.request(right) do
+          :ok -> eat(hunger, left, right, name, controller)
+        end
+    end
+  end
+
+  def eat(hunger, left, right, name, controller) do
+    Chopstick.return(left)
+    Chopstick.return(right)
+    dream(hunger-1, left, right, name, controller)
+  end
+
+  def sleep(0) do :ok end
+  def sleep(time) do
+    :timer.sleep(:random.uniform(time))
+  end
+end
