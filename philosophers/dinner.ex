360f6cb35d93205c5f98831a1d27b9fe01618578
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-24 16:35:21 +0100

    Add dinner hunger size

--- philosophers/dinner.ex
+++ philosophers/dinner.ex
@@ -1,23 +1,28 @@
 defmodule Dinner do
-  def start() do spawn(fn() -> init() end) end
+  def start(hunger) do
+    dinner = spawn(fn() -> init(hunger) end)
+    Process.register(dinner, :dinner)
+  end
 
-  def init() do
+  def init(hunger) do
     chopstick1 = Chopstick.start()
     chopstick2 = Chopstick.start()
     chopstick3 = Chopstick.start()
     chopstick4 = Chopstick.start()
     chopstick5 = Chopstick.start()
     controller = self()
-    Philosopher.start(5, chopstick1, chopstick2, :arendt, controller)
-    Philosopher.start(5, chopstick2, chopstick3, :hypatia, controller)
-    Philosopher.start(5, chopstick3, chopstick4, :simone, controller)
-    Philosopher.start(5, chopstick4, chopstick5, :elisabeth, controller)
-    Philosopher.start(5, chopstick5, chopstick1, :ayn, controller)
-    wait(5, {chopstick1, chopstick2, chopstick3, chopstick4, chopstick5})
+    Philosopher.start(hunger, chopstick1, chopstick2, :arendt, controller)
+    Philosopher.start(hunger, chopstick2, chopstick3, :hypatia, controller)
+    Philosopher.start(hunger, chopstick3, chopstick4, :simone, controller)
+    Philosopher.start(hunger, chopstick4, chopstick5, :elisabeth, controller)
+    Philosopher.start(hunger, chopstick5, chopstick1, :ayn, controller)
+    wait(5, [chopstick1, chopstick2, chopstick3, chopstick4, chopstick5])
   end
 
   def wait(0, chopsticks) do
     Enum.each(chopsticks, fn(stick) -> Chopstick.quit(stick) end)
+    Process.unregister(:dinner)
+    IO.puts("Dinner finished")
   end
   def wait(time, chopsticks) do
     receive do
