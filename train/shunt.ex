177a557049a042cff0810aafa6c424c06ba2ce49
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-28 18:44:00 +0100

    Add few that removes unnecessary moves

--- train/shunt.ex
+++ train/shunt.ex
@@ -10,4 +10,17 @@ defmodule Shunt do
      {:two, -startLength} |
      find(Train.append(startWagons, endWagons), final)]
   end
+
+  def few([], []) do [] end
+  def few([wagon|start], [wagon|final]) do few(start, final) end
+  def few(start, [wagon|final]) do
+    {startWagons, endWagons} = Train.split(start, wagon)
+    startLength = length(startWagons)
+    endLength = length(endWagons)
+    [{:one, endLength+1},
+     {:two, startLength},
+     {:one, -(endLength+1)},
+     {:two, -startLength} |
+     few(Train.append(startWagons, endWagons), final)]
+  end
 end
